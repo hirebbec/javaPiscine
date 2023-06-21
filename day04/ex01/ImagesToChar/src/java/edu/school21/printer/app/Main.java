f4de313e515ddcb79aebc4957e3e790a7ca8ebdc
hirebbec <reek867@gmail.com>
2023-06-21 19:11:06 +0300

    fix

--- day04/ex01/ImagesToChar/src/java/edu/school21/printer/app/Main.java
+++ day04/ex01/ImagesToChar/src/java/edu/school21/printer/app/Main.java
@@ -2,35 +2,18 @@ package edu.school21.printer.app;
 
 import edu.school21.printer.logic.Printer;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.Parameters;
-import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
 
 @Parameters(separators = "=")
 public class Main {
-    @Parameter(names = {"--white"})
-    private static String white;
-    @Parameter(names = {"--black"})
-    private static String black;
+    public static void main(String[] args) {
 
-    private static final String IMAGE_PATH = "/resources/it.bmp";
-
-    public static void main(String... argv) {
-        Main main = new Main();
-        JCommander.newBuilder()
-                .addObject(main)
-                .build()
-                .parse(argv);
-
-        Printer converter = new Printer(white, black);
-
-        try {
-            converter.drawImage(new File(Main.class.getResource(IMAGE_PATH).getFile()));
-        } catch (IOException e) {
-            e.printStackTrace();
+        JCommander 
+        if (args.length != 2 || args[0].length() != 1 || args[1].length() != 1) {
+            System.err.println("Program takes 2 arguments, 2 characters");
+            System.exit(1);
         }
+        Printer printer = new Printer(args[0].charAt(0), args[1].charAt(0));
+        printer.openImage("./target/resources/it.bmp");
+        printer.printImage();
     }
 }
